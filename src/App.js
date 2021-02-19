6d9630581b74f4da3d7639bbbe42cf7a9c405e64
Kyle Long <kylelong@Kyles-MBP.lan>
2021-02-19 00:42:27 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import wineParty from './wine_party.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3>Corked Up</h3>
+       <img id="wineParty" src={wineParty} alt="wine party image"/>
     </div>
   );
 }
