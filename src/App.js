ce12946c90f20cb78a79ecca7bab39ca29cf580a
Kyle Long <kylelong@Kyles-MBP.lan>
2021-02-19 03:33:04 -0500

    Updated styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import wineParty from './wine_party.png';
 import './App.css';
+import './components/LoginForm';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
-    <div className="container">
-      <h3>Corked Up</h3>
-       <img id="wineParty" src={wineParty} alt="wine party image"/>
+    <div className="main">
+          <div className="container">
+            <h3 id="name">Corked Up</h3>
+            <p id="slogan">Helping you have the best wine experience wherever you are.</p>
+            <img id="wineParty" src={wineParty} alt="wine party image"/>
+          </div>
+          <LoginForm />
     </div>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+export default function LoginForm() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off" id="loginForm">
+      <TextField id="outlined-basic" label="Email" variant="outlined" />
+      <TextField id="outlined-basic" label="Password" variant="outlined" />
+      <div class="buttons">
+        <button class="button is-info is-light is-medium is-fullwidth">Login</button>
+        <a href="">Already have an account? Sign in.</a>
+      </div>
+    </form>
+  );
+}
