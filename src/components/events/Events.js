bf3d14f019f8371f063a0c65d59bde9bbc42aea0
Kyle Long <kylelong@Kyles-MacBook-Pro.local>
2022-06-22 05:23:41 -0400

    remove events from frontend

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -3,17 +3,31 @@ import axios from 'axios';
 import EventsHeader from './EventsHeader';
 import Navigation from '../Navigation';
 import SideMenu from '../SideMenu';
-
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 const Events = () => {
     // test with zipCode -> cleanUrls with urls array -> promise array with promises containing clean urls
     // || JSON.parse(window.localStorage.getItem('bars')) !== cleanUrls)
-    const [zipCode, setZipCode] = useState("94109")
+    const [zipCode, setZipCode] = useState("24060")
     const [urlData, setUrlData] = useState([]);
     const [cleanUrls, setCleanUrls] = useState([]);
-// add async await
+    const [zipCodeSave, setZipCodeSave] = useState(false);
+    const [errors, setErrors] = useState({zipcode: ""});
+    const classes = useStyles();
+// get urls
     useEffect(() => {
+        console.log("getting url data...", zipCode)
         axios.get("/test", {params: { zipCode: zipCode} })
         .then((response) => {
+            console.log(response.data)
             setUrlData(response.data)
         })
         .catch((error) => {
@@ -21,17 +35,35 @@ const Events = () => {
         })
 
 
-    }, [])
-    //#endregion
+    }, [zipCode])
+    // clean them 
     const cleanUrl = async (url) => {
         const response = await  axios.get("/cleanUrl", {params: { url: url } });
         const data = await response.data;
         return data;
 
     }
-    //#endregion
+    const onChange = (e) => {
+        const { name, value } = e.target;
+        setZipCodeSave(false);
+        if(name === "zipcode"){
+            if(value.length  === 5){
+                const regEx = /^[0-9]{5}$/;
+                if(value.match(regEx)){
+                    console.log(value);
+                    setZipCode(value);
+                    setErrors(prevErrors => ({ ...prevErrors, zipCode: "" }));
+                }
+                else{
+                    setErrors(prevErrors => ({ ...prevErrors, zipCode: "Zipcode must be 5 digits" }));
+                }
+            }
+        }
+    }
+    // store all clean urls using cleanUrl
     useEffect(() => {
         let urls = []
+        console.log("cleaning urls");
         for(const url of urlData){
             let data = cleanUrl(url);
             if(data != null){
@@ -40,13 +72,19 @@ const Events = () => {
         }
         Promise.all(urls).then((response) => {
             setCleanUrls(response);
+            console.log(response, cleanUrls);
             let bars = JSON.parse(window.localStorage.getItem('bars'));
-            console.log(JSON.parse(window.localStorage.getItem('bars')), response);
-            if( bars === null || bars === undefined || bars.length == 0) {
-                console.log(response);
-                window.localStorage.setItem('bars', JSON.stringify(response));
-                console.log(window.localStorage.getItem('bars') );
-            }
+            //console.log(JSON.parse(window.localStorage.getItem('bars')), response);
+            // if( bars === null || bars === undefined || bars.length == 0) {
+            //    // console.log(response);
+            //     window.localStorage.setItem('bars', JSON.stringify(response));
+            //     //console.log(window.localStorage.getItem('bars') );
+            // }
+            // if(response.length > 0 && response !== bars) {
+            //     console.log(response);
+            //     window.localStorage.removeItem('bars');
+            //     window.localStorage.setItem('bars', JSON.stringify(response));
+            // }
             
 
         }).catch(err => {
@@ -62,15 +100,30 @@ const Events = () => {
             <Navigation />
             <EventsHeader />
             <SideMenu />
+            <div className="wineBarsContainer">
+                        <br />
+                        <form className={classes.root} noValidate autoComplete="off" id="zipCodeForm" onSubmit={e => {e.preventDefault();}}>
+                            <label htmlFor="zipcode">Zipcode</label>
+                                <TextField id="outlined-basic" label={zipCode} variant="outlined" name="zipcode" id="zipcode" placeholder={zipCode}  inputProps={{ maxLength:5 }} onChange={onChange}  />
+                                {(errors.zipcode.length > 0) &&
+                                <p className="error">{errors.zipCode}</p>
+                                }
+                                {/* <div className="buttons">
+                                    <button className="button is-info is-light is-medium is-fullwidth" onClick={updateZipcode}>Update</button>
+                                </div> */}
+                                {zipCodeSave &&
+                                    <span style={{color: "green"}}>Zip Code successfully saved.</span>
+                                }
+            
+                        </form>         
+                    </div>
             <div>
                 { 
    
-                (JSON.parse(window.localStorage.getItem('bars')) === undefined || 
-                JSON.parse(window.localStorage.getItem('bars')) === null || 
-                JSON.parse(window.localStorage.getItem('bars')).length == 0 ) ? 
+                cleanUrls.length === 0 ? 
                 <p>Loading...</p> :
                 
-                JSON.parse(window.localStorage.getItem('bars')).map((url, i) => {
+                cleanUrls.map((url, i) => {
                     return (
                         <p key={i}>{url}</p>
                     )
